ca171914e55f36377daf9922978f4066156a4934
Elkhan <74042126+ubbeg2000@users.noreply.github.com>
2023-03-14 14:50:54 +0700

    Update main.c

--- main.c
+++ main.c
@@ -9,52 +9,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// urutkan nilai dari terbesar ke terkecil
-int sortingNilai(..,..){
-    // Lengkapi fungsi berikut ini
-    // boleh menambah parameter bila diperlukan
-    sortingNilai(..);
+#include <stdio.h>
+#include <stdlib.h>
+
+int sortingNilai(int n, int arr[]){
+    int temp[n];
+    if (n == 0){
+        return arr[n];
+    }
+    for (int i = n - 2; i >= 0; i--){
+        if (arr[n-1] > arr[i]){
+            temp[i] = arr[n-1];
+            arr[n-1] = arr[i];
+            arr[i] = temp[i];
+        }
+    }
+    sortingNilai(n-1, arr); 
 }
 
-// menghitung rata-rata nilai
-float mean(..,..){
-    // Lakukan perhitungan rata-rata dengan fungsi rekursif
-    // boleh menambah parameter bila diperlukan
-    // hint: rata-rata = (mean(n-1,..) * (n-1) + ..) / n 
-    // lengkapi rumus diatas atau cari rata-rata dengan cara lainnya
-    // ingat fungsi mean harus dipanggil kembali pada fungsi ini
+float mean(int n, int arr[]){
+    if (n > 0){
+        return (arr[n-1] + (n-1) * mean(n-1, arr)) / n;
+    }
 }
 
-// cari jumlah mahasiswa yang lulus
-// dinyatakan lulus apabila nilai >= rata-rata
-int mahasiswaLulus(..,..,..){
-    // Lengkapi fungsi berikut ini
-    // boleh menambah parameter bila diperlukan
-    mahasiswaLulus(..,..,..);
+int mahasiswaLulus(int n, int arr[], float mean){
+    if (arr[n-1] >= mean){
+        return n;
+    }
+    mahasiswaLulus(n-1, arr, mean);
 }
 
-void dataNilai(.., ..){
-    sortingNilai(..);
-
-    // print urutan nilai
+void dataNilai(int n, int arr[]){
+    sortingNilai(n, arr);
     printf("\nData nilai setelah diurutkan:\n");
-    printf("%d ", ..);
-    
-    // panggil fungsi mean
-    printf("\nRata-rata nilai: %.2f", ..);
-    // panggil fungsi mahasiswaLulus
-    printf("\nJumlah mahasiswa yang lulus adalah %d mahasiswa dari %d mahasiswa", .., ..);
+    for (int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+    float rata = mean(n, arr);
+    printf("\nRata-rata nilai: %.2f", rata);
+    int mahasiswa = mahasiswaLulus(n, arr, rata);
+    printf("\nJumlah mahasiswa yang lulus adalah %d mahasiswa dari %d mahasiswa", mahasiswa, n);
 }
 
 int main(){
     printf("-- Ayo Bantu Erep --\n");
-
+    int banyakMahasiswa;
     printf("\nMasukkan banyaknya mahasiswa: ");
-    printf("Input tidak valid!\n");
+    scanf("%d",&banyakMahasiswa);
+    while (banyakMahasiswa < 1){
+        printf("Input tidak valid!\n");
+        printf("Masukkan banyaknya mahasiswa: ");
+        scanf("%d",&banyakMahasiswa);
+    }
 
-    printf("Masukkan nilai mahasiswa ke-%d: ", ..);
+    int nilai[banyakMahasiswa];
+    for(int i = 0; i < banyakMahasiswa; i++){
+        printf("Masukkan nilai ke-%d: ", i + 1);
+        scanf("%d", &nilai[i]);
+    }
 
-    dataNilai(.., ..); // hanya fungsi dataNilai yang dipanggil pada main
+    dataNilai(banyakMahasiswa, nilai);
     
     return 0;
 }
